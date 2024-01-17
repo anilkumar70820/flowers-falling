4098ae3b58ef83550eafaf00dafafdcaeac0abe3
anilkumar70820 <ak9565191@gmail.com>
2024-01-17 15:31:37 +0530

    add favicon

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,35 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#second_section",
+    pin: true,
+    start: "top top",
+    end: "bottom top",
+    markers: false,
+    scrub: true,
+  },
+});
+
+tl.fromTo(
+  ".section_second",
+  {
+    y:"150%"
+  },
+  {
+    y:"0"
+  }
+);
+
+
+// return () => {
+//   ScrollTrigger.getAll().forEach((instance) => {
+//     instance.kill();
+//   });
+//   gsap.killTweensOf(window);
+// };
+
+
 // const th = gsap.timeline({
 //   scrollTrigger: {
 //     trigger: "#hero_section",
@@ -20,24 +50,3 @@ gsap.registerPlugin(ScrollTrigger);
 //     opacity: 1,
 //   }
 // );
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#second_section",
-    pin: true,
-    start: "top top",
-    end: "bottom top",
-    markers: false,
-    scrub: true,
-  },
-});
-
-tl.fromTo(
-  ".section_second",
-  {
-    opacity: 0,
-  },
-  {
-    opacity: 1,
-  }
-);
-
